bd7452aa8c6f947a31df0a1178ff41ea8db1a5e0
zhaishuang1 <zhais@purdue.edu>
2015-12-03 17:20:25 -0500

    change item

--- zork/item.cpp
+++ zork/item.cpp
@@ -1,20 +1,42 @@
 using namespace std;
 
 #include "item.hh"
-// #include "trigger.hh"
+#include "turnon.hh"
 
-Item::Item(string Name, string Description, string Status, string Writing, string ToString, string Action):
-Base(Name, Description, Status), writing(Writing), toString(ToString), action(Action)
+Item::Item(string Name, string Description, string Status,
+           string Writing):
+    Base(Name, Description, Status), writing(Writing)
 {
 }
 
 Item::~Item()
 {
+    if(turnon != NULL){
+        delete turnon;   
+    }
 }
 
 void Item::print(){
+    cout << "Type: Turnon Object" << endl;
+    if(name.compare(""))
+        cout << "Name = "+name << endl;
+    if(description.compare(""))
+        cout << "Description = "+description << endl;
+    if(status.compare(""))
+        cout << "Status = "+status << endl;
+    if(writing.compare(""))
+        cout << "Writing = "+writing << endl;
+    if(turnon){
+        cout << "Contain turnon:" << endl;
+        turnon->print();
+    }
+    cout << endl;
+    
 }
 
 void Item::add_trigger(Trigger tg){
-	trigger_list.push_back(tg);
+    trigger_list.push_back(tg);
+}
+void Item::set_turnon(Turnon* turnon){
+    this->turnon = turnon;
 }
--- zork/item.hh
+++ zork/item.hh
@@ -7,21 +7,23 @@
 #include <list>
 
 #include "base.hh"
+#include "turnon.hh"
 #include "trigger.hh"
 
 using namespace std;
 
 class Item : public Base
 {
-	list<Trigger> trigger_list;
-	string writing;
-	string toString;
-	string action;
+    list<Trigger> trigger_list;
+    string writing;
+    string action;
+    Turnon* turnon;
 public:
-	Item(string Name, string Description, string Status, string Writing, string ToString, string Action);
-	virtual ~Item();
-	virtual void print();
-	void add_trigger(Trigger tg);
+    Item(string Name, string Description, string Status, string Writing);
+    virtual ~Item();
+    virtual void print();
+    void add_trigger(Trigger tg);
+    void set_turnon(Turnon* turnon);
 };
 
 #endif
