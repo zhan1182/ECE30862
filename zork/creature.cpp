492e25e985cda1aa32e1dd2f026550e896c71ec1
Jinyi Zhang <zhan1182@purdue.edu>
2015-12-02 21:16:38 -0500

    add creature

---
+++ zork/creature.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+#include "creature.hh"
+
+Creature::Creature(string Name, string Description, string Status, string Attack):
+Base(Name, Description, Status), attack(Attack)
+{
+}
+
+Creature::~Creature()
+{
+}
+
+void Creature::print(){
+}
+
+void Creature::add_trigger(Trigger tg){
+	trigger_list.push_back(tg);
+}
+
+void Creature::add_vul(string vul){
+	vul_list.push_back(vul);
+}
--- zork/creature.hh
+++ zork/creature.hh
@@ -15,11 +15,13 @@ class Creature : public Base
 {
 	list<string> vul_list;
 	list<Trigger> trigger_list;
+	string attack;
 public:
-	Creature(string Name, string Description, string Writing, string ToString, string Action);
+	Creature(string Name, string Description, string Status, string Attack);
 	virtual ~Creature();
 	virtual void print();
 	void add_trigger(Trigger tg);
+	void add_vul(string vul);
 };
 
 #endif
